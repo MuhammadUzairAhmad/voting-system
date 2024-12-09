5dc5af9dd544e2eeefc8b13566a359ab03e7d814
Muhammad Uzair Ahmad <135225948+MuhammadUzairAhmad@users.noreply.github.com>
2024-12-09 19:05:02 +0500

    theme

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { createContext, useContext, useState, ReactNode } from 'react';
+
+interface ThemeContextType {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    if (isDarkMode) {
+      document.documentElement.classList.remove('dark');
+    } else {
+      document.documentElement.classList.add('dark');
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
+import { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,30 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: {
+          background: '#1D2B39',
+          background2: '#2C3F4F',
+          text: '#FFFFFF',
+          text2: '#858D92',
+          heading: '#F2F2F2',
+          button: '#2D9CDB',
+          border: '#2D9CDB',
+          line: '#FFFFFF',
+        },
+        light: {
+          background: '#FFFFFF',
+          background2: '#F2F2F2',
+          text: '#000000',
+          text2: '#828282',
+          heading: '#2B2B2B',
+          button: '#1652F0',
+          border: '#2D9CDB',
+          line: '#000000',
+        },
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
